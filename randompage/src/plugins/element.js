5451fab8111884acee46a5983ad60c5158f82192
quartz010 <quartz010@outlook.com>
2020-06-01 15:45:24 +0800

    add file

---
+++ randompage/src/plugins/element.js
@@ -0,0 +1,14 @@
+// 这里没有按需导入
+
+import Vue from 'vue'
+import {
+  Container,
+  Header,
+  Main,
+  Footer
+} from 'element-ui'
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Footer)
